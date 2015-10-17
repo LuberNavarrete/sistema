1ac1fc6b6f7cbf824a1ef7775309e3713996b780
root <root@localhost.localdomain>
2015-10-17 08:42:37 -0430

    Tabla productos y administracion

--- feria/admin.py
+++ feria/admin.py
@@ -2,10 +2,16 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import franquicia
+from .models import franquicia,producto
 
 class franquiciaadmin(admin.ModelAdmin):
 	list_display = ('nombre','descripcion','activo')
 	list_filter = ['activo']
 
 admin.site.register(franquicia,franquiciaadmin)
+
+class productoadmin(admin.ModelAdmin):
+        list_display = ('franquicia','nombre','descripcion','activo')
+        list_filter = ['activo']
+
+admin.site.register(producto,productoadmin)
--- feria/models.py
+++ feria/models.py
@@ -1,9 +1,15 @@
 from django.db import models
 
-# Create your models here.
-
 class franquicia(models.Model):
 	nombre = models.CharField(max_length=50)
 	descripcion = models.TextField(max_length=200)
 	imagen =models.ImageField(upload_to='imagenes', null = True)
 	activo = models.BooleanField(default = 'true')
+
+class producto(models.Model):
+	franquicia = models.foreingkey(franquicia,limit_choices_to={'activo': True})
+        nombre = models.CharField(max_length=50)
+        descripcion = models.TextField(max_length=200)
+        imagen =models.ImageField(upload_to='imagenes', null = True)
+        activo = models.BooleanField(default = 'true')
+
