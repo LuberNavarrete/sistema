4cb0fc8408a962bf1f131e5822796cd99db0acee
Luber Navarrete <navarreteluber@gmail.com>
2015-10-17 15:09:01 -0430

    Agregados campos de busqueda a modelos

--- feria/admin.py
+++ feria/admin.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from .models import franquicia,producto
 
 class franquiciaadmin(admin.ModelAdmin):
+	search_fields = ['nombre','descripcion']
 	list_display = ('nombre','descripcion','activo')
 	list_filter = ['activo']
 
 class productoadmin(admin.ModelAdmin):
+	search_fields = ['nombre','descripcion']
         list_display = ('franquicia','nombre','descripcion','activo')
         list_filter = ['activo']
 
